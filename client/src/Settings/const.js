f23a30b587d65166fe559d4b659eba07200258f9
Aleksey Koleznev <fc.fenomen@mail.ru>
2024-05-26 14:13:43 +0700

    transfer to http

--- client/src/Settings/const.js
+++ client/src/Settings/const.js
@@ -2,7 +2,7 @@
 // export const httpServer = 'http://localhost:5000';
 
 // prodaction
-export const httpServer = 'https://178.172.165.101/koleznev';
+export const httpServer = 'http://178.172.165.101/koleznev';
 
 // herocku
 // export const httpServer = "https://musictherapy96.herokuapp.com";
