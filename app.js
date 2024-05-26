c7d0236cbe60d27c2a92e1b28cb66ff3b630e6ef
Aleksey Koleznev <fc.fenomen@mail.ru>
2024-05-26 13:12:11 +0700

    Redirection to client side

--- app.js
+++ app.js
@@ -65,12 +65,11 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 });
 
-//
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build'));
-// });
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build'));
+});
 
-// app.use(express.static("client/build"));
+app.use(express.static('client/build'));
 
 const server = http.createServer(app);
 
